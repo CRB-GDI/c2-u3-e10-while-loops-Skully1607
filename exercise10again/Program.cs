475d9fce05e87664c34233336c8cbecaab876b83
{Ari} o(≧口≦)o <arianna.rivas@s.capregboces.org>
2024-03-19 08:48:45 -0400

    YO PONE E WINGING Y U NO PICKUP
    I finished everything. :)

--- exercise10again/Program.cs
+++ exercise10again/Program.cs
@@ -21,7 +21,7 @@
             choice = int.Parse(Console.ReadLine());
             Console.WriteLine();
 
-            while (choice != 5)
+            while (choice != 4)
             {
                 if (choice == 1) ;
                 {
@@ -41,12 +41,49 @@
                     Console.WriteLine();
                 }
 
-
                 if (choice == 2) ;
+                {
+                    Console.WriteLine("Loading New Game");
+                    Console.ReadLine();
+                    Console.WriteLine("*************");
+                    Console.WriteLine("Menu");
+                    Console.WriteLine("1 - New game");
+                    Console.WriteLine("2 - Load game");
+                    Console.WriteLine("3 - options");
+                    Console.WriteLine("4 - quit");
+                    Console.WriteLine("*************");
+                    Console.WriteLine();
+
+                    Console.WriteLine("Enter your choice (1,2,3, or 4): ");
+                    choice = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+                }
 
                 if (choice == 3) ;
+                {
+                    Console.WriteLine("Loading Menu");
+                    Console.ReadLine();
+                    Console.WriteLine("*************");
+                    Console.WriteLine("Menu");
+                    Console.WriteLine("1 - New game");
+                    Console.WriteLine("2 - Load game");
+                    Console.WriteLine("3 - options");
+                    Console.WriteLine("4 - quit");
+                    Console.WriteLine("*************");
+                    Console.WriteLine();
+
+                    Console.WriteLine("Enter your choice (1,2,3, or 4): ");
+                    choice = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+                }
 
                 if (choice == 4) ;
+                {
+                    Console.WriteLine("Quit");
+                    Console.ReadLine();
+                    Console.WriteLine();
+                    choice = int.Parse(Console.ReadLine());
+                }
             }
 
         }
